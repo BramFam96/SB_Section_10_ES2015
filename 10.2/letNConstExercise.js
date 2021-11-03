12f1447969e4f253aa1832666dd61d2e0864397f
BramFam96 <mtbramlett25@gmail.com>
2021-11-02 23:24:05 -0500

    first commit

---
+++ 10.2/letNConstExercise.js
@@ -0,0 +1,14 @@
+//1)
+const PI = 3.14
+//2)
+//var and let differ in scope. var is global or functional.
+//let defaults to block scope
+//3)
+//const differs from var similarly to let. It is scoped to its block,
+//however, it is also immutable;
+//4)
+//mutability;
+//5)
+//hoisting refers to javascript's ability to reference functions prior to
+//their declaration; it does so by 'hoisting' function declarations to the
+//top of our scripts;
